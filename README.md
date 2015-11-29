0a7cc94f8838e53b2b7624d4c1f5f688eadaa48a
Stanislav Pidhorskyi <stanislav@podgorskiy.com>
2015-11-28 21:01:17 -0500

    Added license note. Added readme. Added screenshot

---
+++ README.md
@@ -0,0 +1,12 @@
+#[MinimalFEM](https://github.com/podgorskiy/MinimalFEM)
+============================================================================
+
+
+Minimal implementation of FEM
+
+![image1](https://raw.githubusercontent.com/podgorskiy/MinimalFEM/master/example.png)
+
+License
+-------
+
+MinimalFEM is licensed under the MIT License.
--- main.cpp
+++ main.cpp
@@ -1,3 +1,33 @@
+/*MinimalFEM
+
+Author: Stanislav Pidhorskyi (Podgorskiy)
+stanislav@podgorskiy.com
+stpidhorskyi@mix.wvu.edu
+
+The source code available here: https://github.com/podgorskiy/MinimalFEM/
+
+The MIT License (MIT)
+
+Copyright (c) 2015 Stanislav Pidhorskyi
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.*/
+
 #include <Eigen/Dense>
 #include <Eigen/Sparse>
 #include <string>
